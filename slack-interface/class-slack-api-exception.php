a8e32ddd01b8984f8c575a891f668f16ca83272e
Kenneth Van Der Strieckt <49682872+KennethVDS@users.noreply.github.com>
2020-01-31 12:25:29 +0100

    Initial commit

--- slack-interface/class-slack-api-exception.php
+++ slack-interface/class-slack-api-exception.php
@@ -2,12 +2,6 @@
 namespace Slack_Interface;
 
 use Exception;
-
-/**
- * A simple exception class for throwing Slack API errors.
- *
- * @author Jarkko Laine <jarkko@jarkkolaine.com>
- */
 class Slack_API_Exception extends Exception {
 
 }
